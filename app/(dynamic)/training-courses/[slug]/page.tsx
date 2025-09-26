11d900919bdb9c48d12ff874fd4692e55352a97c
haya_alhosain <hayaalhosain.com@gmail.com>
2025-09-27 02:27:37 +0300

    Refactor training courses page component: Rename function to 'Page' for consistency with naming conventions, enhancing clarity and maintainability of the code.

--- app/(dynamic)/training-courses/[slug]/page.tsx
+++ app/(dynamic)/training-courses/[slug]/page.tsx
@@ -1,11 +1,7 @@
 import { getCategoryDetails } from "@/services/services";
 import CategoryPage from "../_components/category-page";
 
-export default async function page({
-  params,
-}: {
-  params: Promise<{ slug: string }>;
-}) {
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
   const categoryData = await getCategoryDetails(slug);
   const { category, courses } = categoryData;
