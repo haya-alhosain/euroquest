3f9d84593438b3d62b092b79395cded5f25a5e6c
haya_alhosain <hayaalhosain.com@gmail.com>
2025-09-27 02:37:50 +0300

    Replace training courses page component with a simple test component: Comment out existing logic and imports, and introduce a basic React component for initial testing purposes, enhancing clarity for future development.

--- app/(dynamic)/training-courses/[slug]/page.tsx
+++ app/(dynamic)/training-courses/[slug]/page.tsx
@@ -1,10 +1,19 @@
-import { getCategoryDetails } from "@/services/services";
-import CategoryPage from "../_components/category-page";
+// import { getCategoryDetails } from "@/services/services";
+// import CategoryPage from "../_components/category-page";
 
-export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
-  const { slug } = await params;
-  const categoryData = await getCategoryDetails(slug);
-  const { category, courses } = categoryData;
+// export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+//   const { slug } = await params;
+//   const categoryData = await getCategoryDetails(slug);
+//   const { category, courses } = categoryData;
 
-  return <CategoryPage category={category} courses={courses} slug={slug} />;
+//   return <CategoryPage category={category} courses={courses} slug={slug} />;
+// }
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      Test
+    </div>
+  )
 }
